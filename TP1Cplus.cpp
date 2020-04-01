bec5022b1b601cc30733143a923b683f926cb811
unknown <imen.hammammi@gmail.com>
2020-04-01 08:29:45 -0400

    Modif personne 1

--- TP1Cplus.cpp
+++ TP1Cplus.cpp
@@ -1,42 +1,60 @@
-// TP1Cplus.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
+// TP1Cplus.cpp : 
 
 #include <iostream>
 
+	int calculerPgcd(int numerateur,int denominateur) {
+		//cette fonction calcule le PGCD de deux valeurs.
+		int temp;
+		if (numerateur > denominateur){
+			temp = numerateur;
+			numerateur = denominateur;
+			denominateur = temp;
+		}
+    	while (numerateur != 0){
+			temp = denominateur % numerateur;
+			denominateur = numerateur;
+			numerateur = temp;
+		}
+		//std::cout << "Le PGCD est " << denominateur << std::endl;
+        return denominateur;
+	}
 
-void main()
-{
-	int a,b,x,t,m;
-	std::cout<<"Entrez le premier entier positif:\t"<<std::endl;
-	std::cin >> a;
-	std::cout<<"Entrez le deuxieme entier positif:\t"<<std::endl;
-	std::cin >> b;
-
-	if (b > a)
+	int calculerPgcdBinaire(int numerateur, int denominateur)
 	{
-		x = b;
-		b = a;
-		a = x;
+		//cette fonction calcule le PGCD binaires de deux valeurs, cetteimplimentation 
+		//calculerPgcdBinaire(numerateur, denominateur)=calculerPgcdBinaire(denominateur,numerateur) 
+		//le premier parametre doit etre plus grand que le deusiéme dans l'appelle de fonction
+		
+		if (numerateur == 0)
+			return denominateur;	
+		else if (((numerateur % 2)!=0) & ((denominateur % 2) != 0)) // les deux impairs
+			return calculerPgcdBinaire(((numerateur - denominateur) / 2), denominateur);
+		else if (((numerateur % 2) == 0) & ((denominateur % 2) == 0)) // les deux pairs
+			return 2*calculerPgcdBinaire(numerateur/2, denominateur/2);
+		else if (((numerateur % 2) != 0) & ((denominateur % 2) == 0)) // le premier param est impair et le deusiéme est pair
+            return calculerPgcdBinaire(numerateur, denominateur/2);
+		else if (((numerateur % 2) == 0) & ((denominateur % 2) != 0)) //le premier param est pair et le deusiéme est impair
+			return calculerPgcdBinaire(numerateur/2, denominateur);
+	}
+	
+
+	int* simplifierFraction(int diviseur, int fraction[]) {
+		//vette fonction simplifie une fraction grâce à un diviseur donné.
+        for(int i = 0; i < 2; ++i) {
+			fraction[i] = fraction[i]/diviseur;
+	    }
+		return fraction;
 	}
 
 
-	while (b != 0)
+	void main()
 	{
-		x = a % b;
-		a = b;
-		b = x;
-	}
-	std::cout << "Le PGCD est " << a << std::endl;
+		// fonction main qui permet la saisie des fraction et l'affichages des résultats de leurs simplifications
+	int *p; int nbrFractions = 0;
+   // p = saisirFractions(&nbrFractions);
+   // afficherFractionsEtSimplifications(p, nbrFractions);
 
+	//calculerPgcdBinaire(a, b)=calculerPgcdBinaire(b, a), le premier param est le plus grand dans cette implimentation
+	//int pc = calculerPgcdBinaire(30, 20);
+	//std::cout << "Le PGCD est " << pc << std::endl;
 }
-
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
-
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
