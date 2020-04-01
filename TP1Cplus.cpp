f4440da4d074ca79561c8b0f35e017460537b93a
moslem ouled sghaier <ouled.sgaier.moslem@gmail.com>
2020-04-01 11:11:04 -0400

    fraction simplification added

--- TP1Cplus.cpp
+++ TP1Cplus.cpp
@@ -1,6 +1,8 @@
 // TP1Cplus.cpp : 
 
 #include <iostream>
+#include <conio.h>
+#include <stdio.h>
 
 	int calculerPgcd(int numerateur,int denominateur) {
 		//cette fonction calcule le PGCD de deux valeurs.
@@ -46,15 +48,54 @@
 		return fraction;
 	}
 
+	int* saisirFractions(int* nbrFractions) {
+		//vette fonction demande aux personnes utilisatrices la quantité de fractions à analyser afind de les saisir
+		int numerateur, denominateur, k = 1;
+		std::cout << "Entrez le nombre de fraction a analyser:\t" << std::endl;
+		std::cin >> *nbrFractions;
+		int *ptrFractions = new int[*nbrFractions * 2];
+		for (int i = 0; i < *nbrFractions * 2; i = i + 2) {
+			do {
+				std::cout << "Entrez le numerateur de la fraction numero " << k << ":\t" << std::endl;
+				std::cin >> numerateur;
+			} while (numerateur <= 0);
+			do {
+				std::cout << "Entrez le denominateur de la fraction numero " << k << ":\t" << std::endl;
+				std::cin >> denominateur;
+			} while (denominateur <= 0);
+
+			*(ptrFractions + i) = numerateur;
+			*(ptrFractions + i + 1) = denominateur;
+			k = k + 1;
+		}
+		return ptrFractions;
+	}
+
+
+
+	void afficherFractionsEtSimplifications(int *ptrFractions, int nbrFractions) {
+		//vette fonction  affiche les fractions ainsi que leur simplifications finales.
+		int pgcd; int fraction[2]; int *p;
+		std::cout << "Affichage des fractions ainsi que de leur simplifications finales :" << std::endl;
+		for (int i = 0; i < nbrFractions * 2; i = i + 2) {
+			pgcd = calculerPgcd(*(ptrFractions + i), *(ptrFractions + i + 1));
+			fraction[0] = *(ptrFractions + i);
+			fraction[1] = *(ptrFractions + i + 1);
+			p = simplifierFraction(10, fraction);
+			std::cout << *(ptrFractions + i) << "/" << *(ptrFractions + i + 1) << "  <<<  "
+				<< *(p + 0) << "/" << *(p + 1) << std::endl;
+		}
+	}
 
 	void main()
 	{
 		// fonction main qui permet la saisie des fraction et l'affichages des résultats de leurs simplifications
 	int *p; int nbrFractions = 0;
-   // p = saisirFractions(&nbrFractions);
-   // afficherFractionsEtSimplifications(p, nbrFractions);
+    p = saisirFractions(&nbrFractions);
+    afficherFractionsEtSimplifications(p, nbrFractions);
 
 	//calculerPgcdBinaire(a, b)=calculerPgcdBinaire(b, a), le premier param est le plus grand dans cette implimentation
 	//int pc = calculerPgcdBinaire(30, 20);
 	//std::cout << "Le PGCD est " << pc << std::endl;
+	getch();
 }
